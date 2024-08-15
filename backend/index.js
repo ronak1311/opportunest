c7ad9826e9269d20b6fbfb2a25bf50c3ecfd5608
Ronakkumar Gandhi <gandhironak6@gmail.com>
2024-08-14 20:47:28 -0400

    Put Cors Origin to env file

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://127.0.0.1:5173',
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }
 
