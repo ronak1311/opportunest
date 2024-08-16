68039f6e33e95d62ede05941ba18f982c2acc594
Ronakkumar Gandhi <gandhironak6@gmail.com>
2024-08-15 23:41:30 -0400

    Profile Page Added with API integration

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-const API_END_POINT = "http://localhost:3000";
-// const API_END_POINT = "https://opportunest.onrender.com";
+// const API_END_POINT = "http://localhost:3000";
+const API_END_POINT = "https://opportunest.onrender.com";
 export const USER_API_END_POINT=`${API_END_POINT}/api/v1/user`;
 export const JOB_API_END_POINT=`${API_END_POINT}/api/v1/job`;
 export const APPLICATION_API_END_POINT=`${API_END_POINT}/api/v1/application`;
